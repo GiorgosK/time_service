8ecb12e590d4e85a3bdd77111e700558fa3687ce
Giorgos Kontopoulos <kongeo@gmail.com>
2019-02-25 10:08:21 +0200

    Initial commit

---
+++ src/Plugin/rest/resource/TimeService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\time_service\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Drupal\Core\Cache\CacheableResponseInterface;
+use Drupal\Core\Cache\CacheableResponseTrait;
+
+/**
+ * Provides a Demo Resource
+ *
+ * @RestResource(
+ *   id = "time_service",
+ *   label = @Translation("Time Service"),
+ *   uri_paths = {
+ *     "canonical" = "/time_service"
+ *   }
+ * )
+ */
+class TimeService extends ResourceBase implements CacheableResponseInterface {
+
+    use CacheableResponseTrait;
+
+    /**
+     * Responds to entity GET requests.
+     * @return \Drupal\rest\ResourceResponse
+     */
+    public function get() {
+        $build =  [
+            '#cache' => [
+                'max-age' => 0,
+            ]
+        ];
+        
+        $message = [
+            'time' => date("Y-m-d H:i:s"),
+            'timezene' => date_default_timezone_get(),
+        ];
+
+        $response = new ResourceResponse($message);
+        $response->addCacheableDependency($build);
+        return $response;
+    }
+}
